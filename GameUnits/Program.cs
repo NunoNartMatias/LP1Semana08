72fb2606db230969cd09af3aa0ffa7bcc5b0c02a
NunoNartMatias <nunosilvamatias@gmail.com>
2023-05-05 19:57:03 +0100

    Created Array for Units and gave values to their variables

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -2,11 +2,19 @@
 
 namespace GameUnits
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Creating an array with 4 Units
+            Unit[] _units = new Unit[]
+            {
+                // Giving values to created units
+                new MilitaryUnit(5, 10, 15),
+                new MilitaryUni(5, 3, 1),
+                new MMilitaryUnit(3, 15, 8),
+                new SettlerUnit(),
+            };
         }
     }
 }
